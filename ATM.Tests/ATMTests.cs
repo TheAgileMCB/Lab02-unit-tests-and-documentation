7cbb800d783cc5f3daa59c1e519d8efcbb0ffe0e
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-06 00:38:00 -0500

    two tests pass

---
+++ ATM.Tests/ATMTests.cs
@@ -0,0 +1,75 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using Xunit;
+using Lab02UnitTests;
+
+namespace ATM.Tests
+{
+    public class ATMTests
+    {
+        //[Fact]
+        //public void Does_balance_exist()
+        //{
+        //    //Arrange
+        //    double balance = 20;
+
+        //    // Act
+        //    double result = Lab02UnitTests.ATM.ViewBalance(balance);
+
+        //    // Assert
+        //    Assert.StrictEqual(balance, result);
+
+        //}
+
+        [Fact]
+        public void  Does_money_come_out()
+        {
+            //Arrange
+            double balance = Lab02UnitTests.ATM.GetBalance();
+            double withdrawal = 10;
+            double testBalance = balance - withdrawal;
+
+            // Act
+            double result = Lab02UnitTests.ATM.WithdrawFunds(withdrawal);
+
+            // Assert
+            Assert.Equal(testBalance, result);
+        }
+
+        [Fact]
+        public void Does_money_go_in()
+        {
+            //Arrange
+            double balance = Lab02UnitTests.ATM.GetBalance();
+            double deposit = 10;
+            double testBalance = balance + deposit;
+
+            // Act
+            double result = Lab02UnitTests.ATM.DepositFunds(deposit);
+
+            // Assert
+            Assert.Equal(testBalance, result);
+        }
+
+
+
+
+
+
+
+
+        //[Fact]
+        //public void overdraftProtection()
+
+        //    //Arrange
+        //decimal originalBalance = Lab02UnitTests.ATM.GetBalance();
+        //decimal amountToWithdraw = originalBalance + 500;
+
+        //// Act
+        //decimal balanceAfterWithdraw = Program.withdrawFunds(amountToWithdraw);
+
+
+        //// Assert
+        //Assert.Equal(orignalBalance, balanceToWithdraw)
+    }
+}
--- Lab02UnitTests/Program.cs
+++ Lab02UnitTests/Program.cs
@@ -2,11 +2,10 @@
 
 namespace Lab02UnitTests
 {
-	public class Program
+	public class ATM
 	{
-		public static void Main(string[] args)
+		public static void Main()
 		{
-			double balance = 20000.00;
 			Console.WriteLine("Hello!");
 			Console.Write(" Welcome, User! \n What would you like to do? \n Please type it now, \n\n Check Balance \n Withdraw \n Deposit \n Exit \n\n");
 
@@ -15,44 +14,76 @@ namespace Lab02UnitTests
 			{
 				case "check balance":
 	
-				viewBalance(balance);
+				ViewBalance();
 					break;
 
 				case "withdraw":
-			
-				withdrawFunds(balance);
+
+					Console.WriteLine("How much would you like to withdraw?");
+					double withdrawal = double.Parse(Console.ReadLine());
+
+					WithdrawFunds(withdrawal);
 					break;
 
 				case "deposit":
 
-				depositFunds(balance);
+					Console.WriteLine("How much would you like to deposit?");
+					double deposit = double.Parse(Console.ReadLine());
+
+					DepositFunds(deposit);
 					break;
 
 				default:
 				Console.WriteLine("Thank you and goodbye!");
 					break;
 			}
-
 		}
 
-		public static void viewBalance(double balance)
+		public static void ViewBalance()
 		{
 			Console.WriteLine($"Your balance is {balance:C}");
 
 		}
+		private static double balance = 20000;
 
-		public static double withdrawFunds(double balance)
+		public static double GetBalance()
 		{
-			Console.WriteLine("How much would you like to withdraw?");
-			double withdrawl = double.Parse(Console.ReadLine());
-			double newBalance = balance - withdrawl;
-			Console.WriteLine($"You are withdrawing {withdrawl:C}");
-			return newBalance;
+			return balance;
 		}
 
-		public static double depositFunds(double balance)
+		public static double WithdrawFunds(double withdrawal)
 		{
-			return 00.00;
+			try
+			{
+				double newBalance = balance - withdrawal;
+				if(newBalance < 0)
+				{
+					Console.WriteLine("Your account must remain positive");
+					throw new Exception("Your account must remain positive.");
+				}
+				Console.WriteLine($"You are withdrawing {withdrawal:C}");
+				return newBalance;
+			}
+			catch (ArgumentOutOfRangeException)
+			{
+
+				throw;
+			}
+		}
+
+		public static double DepositFunds(double deposit)
+		{
+			try
+			{
+				double newBalance = balance + deposit;
+				Console.WriteLine($"You are depositing {deposit:C}");
+				return newBalance;
+			}
+			catch (ArgumentOutOfRangeException)
+			{
+
+				throw;
+			}
 		}
 	}
 }
